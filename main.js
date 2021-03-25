4f4f86e506a4fdf23e936754d840c3b4dc08b150
Mateusz Walęcki <m.walecki1992@o2.pl>
2021-03-25 14:30:05 +0100

    better colors

--- main.js
+++ main.js
@@ -2,5 +2,11 @@ const firstName = "Mateusz";
 const rocznik = 1992;
 
 console.log(
-  `Witam, mam na imie ${firstName} i urodziłem się w ${rocznik} roku. `
+  `Witam, mam na imie ${firstName} i urodziłem się w ${rocznik} roku, jestem tu bo ucze się frontendu :). `
 );
+
+
+
+const heading = document.querySelector('.js-text');
+
+heading.innerHTML = `Witam, mam na imie ${firstName} i urodziłem się w ${rocznik} roku, jestem tu bo ucze się frontendu :) `
