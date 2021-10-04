454d09da229993bead73977d4f4c8a6d7f594f2e
Mateusz Walęcki <m.walecki1992@o2.pl>
2021-10-04 21:48:53 +0200

    hamburger menu

--- main.js
+++ main.js
@@ -183,3 +183,8 @@ myRange.value */
 
 
 
+const hamburger = document.querySelector('.hamburger--js');
+hamburger.addEventListener('click', () => {
+  const nav = document.querySelector('.navigation--js');
+  nav.classList.toggle('navigation--open');
+})
